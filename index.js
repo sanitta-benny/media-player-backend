3b7d0f2ddbeba3828c4c5bef648c406c61148913
sanitta-benny <sanittavattakuzhy@gmail.com>
2023-11-09 10:56:08 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/* import json -server library in index,js file */
+const jsonServer=require('json-server')
+
+
+/* create json-server */
+const mediaPlayerServer=jsonServer.create()
+
+
+/* create path to json file  */
+ const router=jsonServer.router('db.json')
+
+
+ /* middleware to convert json to js  */
+ const middleware=jsonServer.defaults()
+
+
+ /* use middleware and router in server */
+ mediaPlayerServer.use(middleware)
+ mediaPlayerServer.use(router)
+ 
+ /* setup port for running server */
+ const port=5000 || process.env.port
+
+
+ /* to  listen server for resolving request from client*/
+ mediaPlayerServer.listen(port,()=>{
+    console.log(`mediaPlayerServer started at ${port} and waiting for the request`)
+ })
